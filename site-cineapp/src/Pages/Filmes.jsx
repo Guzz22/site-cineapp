cf7f46d045282266e446c04ca363be1c3da1f982
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-29 12:29:01 -0300

    a

--- site-cineapp/src/Pages/Filmes.jsx
+++ site-cineapp/src/Pages/Filmes.jsx
@@ -1,23 +1,9 @@
-import data from '../../artigos.json'
-
 function Filmes() {
-    console.log(data)
     return (
         <> 
         <h1> Filmes </h1>
-        {
-data.map(
-    filme => (
-        <>
-        <h1 key={filme.title}>{filme.title} </h1>
-        <img src={filme.image}/>
-        </>
-                )
-
-            )
-        }
-        </>
-     )
+       </>
+    )
 }
 
 export default Filmes;
--- site-cineapp/src/Pages/Home.jsx
+++ site-cineapp/src/Pages/Home.jsx
@@ -1,7 +1,34 @@
+import data from '../../artigos.json'
+
 function Home() {
     return ( 
-        <h1> Home </h1>
-     );
+       
+        <>
+
+        <div className='grid grid-cols-4'>
+        {data.map(
+            (filme, index) => (
+                <div className='card' key={index}>
+                    <h1>{filme.title}</h1>
+                    <img src={filme.image}/>
+                    <div className="tags">
+                        
+                        {filme.tags.map(tag => (
+                            <span className="bg-pink-800  text-white p-1 m-1" key={tag}>{tag}</span>
+                        ))}
+                        
+                    </div>
+    
+                </div>
+            )
+
+        )}
+        </div>
+       
+
+        </>
+      
+    );
 }
 
 export default Home;
