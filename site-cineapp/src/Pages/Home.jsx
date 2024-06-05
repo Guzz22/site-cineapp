0b84829caa2ece5f637ecd055cd9b1d3bc4d065a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-05 09:38:18 -0300

    ATAULIZAMOS PAGINA FILMES

--- site-cineapp/src/Pages/Home.jsx
+++ site-cineapp/src/Pages/Home.jsx
@@ -5,20 +5,26 @@ function Home() {
        
         <>
 
-        <div className='grid grid-cols-1  md:grid-cols-3'>
+        <div className='grid grid-cols-3 gap-3'>
         {data.map(
             (filme, index) => (
                 <div className='card' key={index}>
                     <h1>{filme.title}</h1>
-                    <img  src={filme.image}/>
+                    <img className="mb-2" src={filme.image} alt={filme.title}/>
                     <div className="tags">
                         
                         {filme.tags.map(tag => (
-                            <span className="bg-pink-800  text-white p-1 m-1" key={tag}>{tag}</span>
+                            <span className="bg-green-800  text-black p-1 m-1" key={tag}>{tag}</span>
                         ))}
                         
                     </div>
-    
+    <div className="texto">
+        {filme.text.map((texto, tag)=>(
+            <p key={tag}>{texto}</p>
+        ))}
+
+
+    </div>
                 </div>
             )
 
